2b3c1da87c44d8d18e1bb0307c71089429876e57
Vishal Chhodwani <vishal.chhodwani@ibm.com>
2020-07-22 11:05:54 +0530

    changes

--- App.js
+++ App.js
@@ -1,137 +1,61 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  Button,
-  TouchableOpacity
-} from 'react-native';
-import LifeCycle from './LifeCycle';
+import {View, Text, TouchableOpacity} from 'react-native';
+import ChildComponent from './ChildComponent';
 
-export class  App extends React.Component {
-
+export default class App extends React.Component {
 
   constructor(props){
     super(props)
 
-    console.log('App', "constructor");
-
+    console.log('App', 'constructor');
     this.state = {
-      isChildVisible: false,
-      childName: "Mike",
-      childAge: 10,
+      isVisible: false,
+      name: "Mike",
+      age: 10,
     }
   }
 
-
-  static getDerivedStateFromProps(props, state){
-      console.log('App', "getDerivedStateFromProps");       
-      
-      return null; 
+  onChildComponentShow(){
+    this.setState({isVisible: true});
   }
 
-  componentDidMount(){
-      console.log('App', "componentDidMount");
+  onChildComponentHide(){
+    this.setState({isVisible: false});
   }
 
-  shouldComponentUpdate(nextProps, nextState){
-      console.log('App', "shouldComponentUpdate");
-      return true;
+  onPropsChanged(){
+    this.setState({name: "Peter", age: 12});
   }
-
-  getSnapshotBeforeUpdate(prevProps, prevState){
-      console.log('App', "getSnapshotBeforeUpdate");
-
-      return null;
-  }
-
-  componentDidUpdate(){
-      console.log('App', "componentDidUpdate");
-  }
-
-  componentWillUnmount(){
-      console.log('App', "componentWillUnmount");
-  }
-
-  showChildComponent(){
-   this.setState({isChildVisible: true})
- }
- 
-  hideChildComponent(){
-    this.setState({isChildVisible: false});
-  }
-
-  changeProps(){
-    this.setState({childName: "Peter", childAge: 12});
-  }
+  
 
   render(){
-    console.log('App', "render");
-    return (
-      <View style={styles.view}>
-        <Text style={styles.text}>Hello {this.state.name} Welcome to the React Native World</Text>
-        
-        
-        <TouchableOpacity style={styles.customButton} onPress={()=> this.showChildComponent()}>
-          <Text style={[styles.buttonText, {color: "white"}]}>Show Child Component</Text>
+    console.log('App', 'render');
+    return(
+      <View>
+        <Text>Hello Welcome to react native world</Text>
+
+        <TouchableOpacity style={{marginTop:20, backgroundColor: 'blue', padding: 10, justifyContent: 'center', alignItems:'center'}} 
+        onPress={()=> this.onChildComponentShow()}>
+          <Text style={{color: 'white', fontSize: 18}}>Show ChildComponent</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={[styles.customButton, {backgroundColor: 'orange'}]} onPress={()=> this.hideChildComponent()}>
-          <Text style={[styles.buttonText, {color: "black"}]}>Hide Child Component</Text>
-        </TouchableOpacity>
         
-        <TouchableOpacity style={[styles.customButton, {backgroundColor: 'green'}]} onPress={()=> this.changeProps()}>
-          <Text  style={[styles.buttonText, {color: "white"}]}>Change Props</Text>
+        <TouchableOpacity style={{marginTop:20, backgroundColor: 'blue', padding: 10, justifyContent: 'center', alignItems:'center'}} 
+        onPress={()=> this.onChildComponentHide()}>
+          <Text style={{color: 'white', fontSize: 18}}>Hide ChildComponent</Text>
+        </TouchableOpacity>
+
+        
+        <TouchableOpacity style={{marginTop:20, backgroundColor: 'blue', padding: 10, justifyContent: 'center', alignItems:'center'}} 
+        onPress={()=> this.onPropsChanged()}>
+          <Text style={{color: 'white', fontSize: 18}}>Change Props</Text>
         </TouchableOpacity>
 
         {
-          this.state.isChildVisible != "" &&
-          <View style={{marginTop: 50}}>
-            <LifeCycle name={this.state.childName} age={this.state.childAge}/>
-          </View>
+          this.state.isVisible &&
+          <ChildComponent name={this.state.name} age={this.state.age}/>
         }
       </View>
-      );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  view: {
-    flex: 1,
-    backgroundColor: 'white'
-  },
-  text: {
-    fontSize: 22,
-    color: 'red',
-    alignSelf: 'center',
-    marginTop: 20,
-    marginBottom: 20,
-    paddingLeft: 10
-  },
-  button: {
-    marginTop:20
-  },
-  customButton: {
-    backgroundColor: 'blue',
-    justifyContent: 'center',
-    alignItems: "center",
-    padding: 10,
-    marginTop: 20
-  },
-  buttonText:{
-    fontSize: 18,
-    fontWeight: 'bold'
-  }
-});
-
-export default App;
---
+++ ChildComponent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default class ChildComponent extends React.Component{
+
+    constructor(props){
+        super(props)
+        console.log('ChildComponent', "constructor");
+    }
+
+    render(){
+        console.log('ChildComponent', "render");
+        return(
+            <View >
+                <Text style={{fontSize: 40, color: 'green', marginTop: 10, padding: 10}}>
+                    I am a child component. My name is {this.props.name} and I am {this.props.age} years old
+                </Text>
+            </View>
+        )
+    }
+}
